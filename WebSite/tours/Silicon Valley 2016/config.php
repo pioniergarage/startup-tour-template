d7e56532cc1d8abec0dcb016aacf23197df75b3d
domiko96 <domiko96@gmail.com>
2018-07-12 20:37:40 +0200

    added pseudo content for other tours

---
+++ WebSite/tours/Silicon Valley 2016/config.php
@@ -0,0 +1,52 @@
+<?php
+//Visited Location/Area -> Title
+$Location = "India 2017";
+$DistanceToKarlsruhe = "2000";
+
+//Change the Background trough replace /img/Background
+
+//E-Mail for Contact Button, if not set - Contact-Button disapiers
+// $ContactEmail = "dominik.kogel@pioniergarage.de";
+
+//Text which sumerizes the Tour in about 200-300 Words suggested
+$AboutTourText = "The Pioneers are heading to India! After inspiring tours to Shanghai and Peking, the Silicon Valley, Boston and New York, London, Tel Aviv, Berlin and Munich the PionierGarage is visiting different cities in India. We are full of energy to find out more about the entrepreneurial scene in Bangalore and Pune and to get in touch with a variety of interesting and inspiring people. During the 10-day tour, we are visiting a number of startups, venture capitalists, entrepreneurial organizations and educational institutions.";
+
+//Text next to the PG Logo
+$AboutThePGText = "15 students from the PionierGarage, the university group for entrepreneurship at the Karlsruhe Institute of Technology (KIT) in Germany, are going to visit one of the world's most significant tech and startup regions: India.
+To find out more about our entrepreneurship club at Germany's leading technical university, head over to our homepage or contact us on info @ pioniergarage.de to become part of the tour as a supporter, partner, sponsor or Startup/VC to be visited. We're looking forward to meeting inspiring people!.";
+
+//List of Visited Startups, Names Used to find the logo"s in /img/StartupLogos/StartupName.pioniergarage
+//Store Startup Logos int the Form "StartupName.png" like in the example here
+// Images should have the Size 300x200 Pixel
+$VisitedStartups = array("Google","Uber","SpaceX","SpaceX");
+$AdditionalText = array("Monday 20.11.22", "Car Firm, 21.11.22", "22.11.22", "22.11.22");
+
+
+
+//Our TeamText - sumerize 30 Words
+$TeamText = "We are Students ...";
+
+//Team - Images in /img/team/ImgName.png (120x120px images, as png), Text about 20-40 Words
+//CopyElements for more People
+$TeamMember[] = array('Name' => "Tobi",
+                                                'ImgName' => "c4.jpg",
+                                                'Text'   => "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam");
+$TeamMember[] = array('Name' => "Markus",
+                                                'ImgName' => "c5.jpg",
+                                                'Text'   => "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam");
+$TeamMember[] = array('Name' => "Muster",
+                                                'ImgName' => "c6.jpg",
+                                                'Text'   => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed nisl non tellus rutrum tincidunt. Etiam varius, elit eget volutpat.");
+
+//Sponsors - Images in /img/Sponsors/ImgName.png, 300x200px images
+$Sponsors[] = array('ImgName' => "daimler.png",
+                                        'Link'   => "https://www.daimler.com/de/");
+$Sponsors[] = array('ImgName' => "sovendus.png",
+                                        'Link'   => "https://www.sovendus.de/de/");
+
+//Youtube Link - if empty -> nothing is shown
+$YoutubeLink = "https://www.youtube.com/watch?v=t-WDfZML1FY";
+
+
+//echo "debug";
+?>
