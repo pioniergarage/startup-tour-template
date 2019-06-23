2bf56cca3e40165842136f75a9f7f6b1414dcbe7
Kevinkrs <Kevin@Kevins-MBP-2.fritz.box>
2019-06-23 18:03:46 +0200

    Update Shanghai2019

---
+++ WebSite/tours/Shanghai 2019/config.php
@@ -0,0 +1,137 @@
+<?php
+
+//Visited Location/Area -> Title
+$Location = "Shanghai<br>2019";
+//$KilometersTraveled = ;
+//$DaysAbroad = ;
+//Change the Background trough replace /img/Background
+
+//E-Mail for Contact Button, if not set - Contact-Button disapiers
+// $ContactEmail = "kevin.kraus@pioniergarage.de";
+
+//Text which sumerizes the Tour in about 200-300 Words suggested
+$AboutTourText = 'China. As the Country with the bigest population on earth, it has to tackel a lot of social, 
+ economic and ecological issues. Nevertheless, as one of the most triving economies in the world,
+ China is growing fast and innovative. Very competetive as they are, there is a lot of space for 
+ young startups, to start there business and develope there ideas. The growth, of the economy of China for the past years
+ is stunning! If you‘re in contact with any Startups, you may think are essantial for our Startuptour, feel free
+ to get in touch with us!
+                    <h4>Timeline</h4>
+                    <ul class="pg-timeline">
+                        <li>
+                            <h5>Shanghai</h5>
+                            <p>11 September 2019 – 18 September 2019</p>
+                        </li>
+                        <li>
+                            <h5>Hangzhou</h5>
+                            <p>18 September 2019 – 22 September 2019</p>
+                        </li>
+                        <li>
+                            <h5>Bejing</h5>
+                            <p>22 September April 2019 – 29 September 2019</p>
+                        </li>
+                    </ul>';
+
+//Text next to the PG Logo
+$AboutThePGText = "10 members of PionierGarage, Germany‘ largest university club focused on entrepreneurship, located at the Karlsruhe Institute of Technology (KIT) in Germany, are going to visit three of the world‘s bigest and innovative cities: Shanghai, Hangzouh and Bejing.
+To find out more about our entrepreneurship club at Germany‘s leading technical university, head over to our homepage or contact us on <a href='mailto:info@pioniergarage.de'>info@pioniergarage.de</a>  to become part of the tour as a supporter, partner, sponsor or Startup/VC to be visited. We‘re looking forward to meeting inspiring people!";
+
+//List of Visited Startups, Names Used to find the logo"s in /img/StartupLogos/StartupName.pioniergarage
+//Store Startup Logos int the Form "StartupName.png" like in the example here
+// Images should have the Size 300x200 Pixel
+// $StartupsToVisit = ;
+// $NumberOfStartups = ;
+
+
+//Our TeamText - sumerize 30 Words
+$TeamText = "We are a team of young founders and entrepreneurial students.";
+
+//Team - Images in /img/team/ImgName.png (120x120px images, as png), Text about 20-40 Words
+//CopyElements for more People
+$TeamMember[] = array(
+    'Name' => "Elisabeth Goebel",
+    'ImgName' => "elisa_goebel.jpg",
+    'Studies' => "Member of Human Ressources and industrial engineering student at KIT",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+$TeamMember[] = array(
+    'Name' => "Marvin Wach",
+    'ImgName' => "marvin_wach.jpg",
+    'Studies' => "Head of External Relations, studying Civil Engineering in the fifth Bachelor Semester",
+    'Text'   => "China is a continent with a very young average population-age. The potential in the startup-scene is huge. Therefore we want to take the chance to meet up with one of the greatest entrepreneur in Africa.",
+    'Linkedin' => 'https://www.linkedin.com/in/marvin-wach-0b1736174/'
+);
+$TeamMember[] = array(
+    'Name' => "Tobias Budig ",
+    'ImgName' => "tobias_budig.jpg",
+    'Studies' => "",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+$TeamMember[] = array(
+    'Name' => "Jannik Nefferdorf",
+    'ImgName' => "jannik_nefferdorf.jpg",
+    'Studies' => "Former board member of PionierGarage and Industrial Engineering student at KIT Karlsruhe",
+    'Text'   => "As an entrepreneurship enthusiast, I love talking to founders. I'm really looking forward to our China startup tour to learn about the challenges and opportunities of developing new business ideas in China.",
+    'Linkedin' => 'https://www.linkedin.com/in/neffi97/'
+);
+$TeamMember[] = array(
+    'Name' => "Kevin Kraus",
+    'ImgName' => "kevin_kraus.jpeg",
+    'Studies' => "CMO @PionierGarage, Industrial Engineering student at KIT Karlsruhe",
+    'Text'   => "China, as a thriving culture and economy, is a very fascinating country. I'm very excited to learn more about the old culture and the entrepreneurial spirit of China!",
+    'Linkedin' => 'https://www.linkedin.com/feed/'
+);
+ $TeamMember[] = array(
+    'Name' => "Jasmin Riedel",
+    'ImgName' => "",
+    'Studies' => "",
+    'Linkedin' => ''
+);    
+$TeamMember[] = array(
+    'Name' => "Jan Bode",
+    'ImgName' => "",
+    'Studies' => "",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+$TeamMember[] = array(
+    'Name' => "Han Völker",
+    'ImgName' => "",
+    'Studies' => "",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+$TeamMember[] = array(
+    'Name' => "Tobias Budig",
+    'ImgName' => "",
+    'Studies' => "",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+$TeamMember[] = array(
+    'Name' => "Jan Effenberger",
+    'ImgName' => "",
+    'Studies' => "",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+$TeamMember[] = array(
+    'Name' => "Marcus Maddock",
+    'ImgName' => "",
+    'Studies' => "",
+    'Text'   => "",
+    'Linkedin' => ''
+);
+
+//Sponsors - Images in /img/Sponsors/ImgName.png, 300x200px images                                       'Link'   => "https://www.karlsruhe.de/");
+$Sponsors[] = array('ImgName' => "Logo_Karlsruhe.png",
+                                        'Link'   => "https://www.karlsruhe.de/");
+$Sponsors[] = array('ImgName' => "Logo_KIT.svg",
+                                        'Link'   => "https://www.kit.edu/");
+
+//Youtube Link - if empty -> nothing is shown
+$YoutubeLink = "";
+
+//echo "debug";
